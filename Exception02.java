5dbcf04aef71f6c937089b2750c1543d820906bb
kaito <73706481+kaigitt@users.noreply.github.com>
2021-04-04 22:08:52 +0900

    initial commit

---
+++ Exception02.java
@@ -0,0 +1,14 @@
+package exception;
+
+public class Exception02 {
+  public static void main(String args) {
+    try {
+      System.out.println("100 + 0は？");
+      int result = 100 / 0;
+    } catch (ArithmeticException e) {
+      System.out.println("例外が発生");
+    } finally {
+      System.out.println("プログラム終了");
+    }
+  }
+}
