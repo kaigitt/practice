4bda6bf741604f04fdc8c75c33372d5a9dd14d41
kaito <73706481+kaigitt@users.noreply.github.com>
2021-04-11 23:21:17 +0900

    practice

---
+++ paiza/to_big_str.rb
@@ -0,0 +1,9 @@
+nums = gets.chomp.split(' ')
+str = gets.chomp
+
+(1..(str.size)).each do |i|
+  if nums[0].to_i <= i && i <= nums[1].to_i
+    print str[i - 1].upcase
+  else
+    print str[i - 1]
+  end
