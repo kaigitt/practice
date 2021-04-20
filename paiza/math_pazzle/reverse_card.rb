72e5af52596c9d9075d9772396cfa51eedda59e5
kaito <73706481+kaigitt@users.noreply.github.com>
2021-04-20 19:55:34 +0900

    practice

--- paiza/math_pazzle/reverse_card.rb
+++ paiza/math_pazzle/reverse_card.rb
@@ -2,14 +2,14 @@ N = 100
 cards = Array.new(N, false)
 p cards
 
-# (2..N).each{ |i|
-#   j = i - 1
-#   while (j < cards.size) do
-#     cards[j] = !cards[j]
-#     j += 1
-#   end
-# }
+(2..N).each{ |i|
+  j = i - 1
+  while (j < cards.size) do
+    cards[j] = !cards[j]
+    j += 1
+  end
+}
 
-# N.times do |i|
-#   puts i + 1 if !cards[i]
-# end
+N.times do |i|
+  puts i + 1 if !cards[i]
+end
