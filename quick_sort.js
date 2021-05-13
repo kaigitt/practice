5d735d0482dee751ba50214fecefa8c867a9b09d
kaito <73706481+kaigitt@users.noreply.github.com>
2021-05-13 22:49:11 +0900

    practice

---
+++ quick_sort.js
@@ -0,0 +1,15 @@
+const quickSort = (arr) => {
+  if (arr < 2) {
+    return arr;
+  }
+  const pivot = arr[0];
+
+  const minArr = arr.filter(arr => arr < pivot);
+  const maxArr = arr.filter(arr => arr > pivot);
+
+  return [...quickSort(minArr), pivot, ...quickSort(maxArr)];
+}
+
+const arr = quickSort([8,41,6,4,2,19]);
+
+console.log(arr);
